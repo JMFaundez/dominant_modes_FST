d084de25c88796117cc0cb3d7332fb26c0a098f3
josework <josfa@mech.kth.se>
2021-05-05 12:30:17 +0200

    from felberg

--- evaluate_noenvelope2.m
+++ evaluate_noenvelope2.m
@@ -14,17 +14,17 @@ for i=1:length(modes)
     freq = -ft(mi(2))/(2*pi);
     for j=1:length(xfvec)
         xf = xfvec(j);
-        [xw1,q1,y_opt1,En1,bbb,xar] = streak_readBL(xf,0.005,beta,freq,0,N,q0);
+        [xw1,q1,y_opt1,En1,bbb,xar] = streak_readBL(xf,0.02,beta,freq,0,N,q0);
 
         q0 = q1(:,1);
-        [xwj,qj,y_optj,Enj,bbb2,xarc2] = streak_readBL_no_opt(0.34,0.005,beta,freq,0,N,q0,y_opt1);
+        [xwj,qj,y_optj,Enj,bbb2,xarc2] = streak_readBL_no_opt(0.34,0.02,beta,freq,0,N,q0,y_opt1);
         xw{j} = xwj;
         q{j} = qj;
         En{j} = Enj;
         y_opt{j} = y_optj;
 
     end
-    save(['optw_mode',num2str(i),'_difx.mat'],'xw','q','En','y_opt');
+    save(['optw2_mode',num2str(i),'_difx.mat'],'xw','q','En','y_opt');
 end
 
 
